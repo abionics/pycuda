fc41fd5f2180cb4c3962967311c85f22a6c1f4e3
Andreas Kloeckner <inform@tiker.net>
2026-06-23 14:19:02 -0400

    Silence ruff warning about autouse fixture

--- test/test_gpuarray.py
+++ test/test_gpuarray.py
@@ -14,7 +14,7 @@ from pycuda.compiler import SourceModule
 from pycuda.tools import init_cuda_context_fixture
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(autouse=True)  # noqa: RUF076
 def init_cuda_context():
     yield from init_cuda_context_fixture()
 
